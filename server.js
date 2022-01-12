b6e56fd6aaed28c8d993537ed41984222e02d6b2
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-01-12 11:02:36 -0800

    added express server / handlebars communicate with express

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 // where the main server lives
+const express = require('express');
+exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({});
+
+// set up express app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sey Handlebbars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.listen(PORT, () => {
+    console.log('Server listening on https://localhost:' + PORT);
+});
