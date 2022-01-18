3c60064c5b59525ff97fbe9b0c0b7585642bb67f
SantiBass <sibarraryan@gmail.com>
2022-01-17 16:43:39 -0800

    created model/index.js

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const seedUsers = require('./user-seed');
+const seedPosts = require('./search-seed');
+const seedComments = require('./history-seed');
+
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('--------------sequelize');
+  await seedUsers();
+  console.log('--------------seedUser');
+
+  await seedPosts();
+  console.log('--------------seedPost');
+
+  await seedComments();
+  console.log('-------------- seedComments');
+
+  
+  process.exit(0);
+};
+
+seedAll();
